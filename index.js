3cb3bf6b6ba0c2844373dd9988c7c2c2a2556f0c
carlobearr <carlo_luis_v_santos@dlsu.edu.ph>
2020-03-18 14:16:23 +0800

    forum updates

--- index.js
+++ index.js
@@ -18,14 +18,10 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(__dirname + '/public'));
 var usernames= "Default";
 var ids= "Default";
-app.get('/profile', function(req,res){res.render('profile',{
-    title: 'About Me',
-    name: 'Carlo Luis Santos',
-    id: '118',
-    degree: 'BSCS-ST',
-    bio: 'Carlo is a student of the College of Computer Studies and is very active member of multiple organizations.'
-})})
 app.get('/', function(req,res){res.render('index',{})})
+app.get('/post/:id(\d+)', function(req,res){res.render('post',{
+    ids: req.params.id,
+})})
 app.get('/post', function(req,res){res.render('post',{
     forumtitle: '[Martial Law] What if Marcos never declared Martial Law?',
     forumdate: 'February 28, 2020',
@@ -33,18 +29,6 @@ app.get('/post', function(req,res){res.render('post',{
     forumpost:"What if Marcos never declared Martial Law and stepped down in '73? Would we have seen a President Ninoy Aquino? Would we have been a vastly different country today (socially and/or economically), or would it be more of the same? I'd like to know your thoughts on this.",
     forumreact: 999,
     commentcount: 2
-})})
-app.get('/activities', function(req,res){res.render('activities',{
-    title: 'Extra Curricular Activities',
-    orgs: [{
-        name: 'Alyansang Tapat Sa Lasallista',
-        position:'College of Computer Studies Governor'},
-        {
-        name: 'Computer Studies Government',
-        position:'Chief of Staff'},
-        {
-        name: 'La Salle Computer Society',
-        position:'Associate Vice President Human Resource Development'}],
 })})
 app.listen(port, function(){
     console.log('App listening at port ' + port)
