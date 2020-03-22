ab2cad64ee81951a9e345b587fa14766a11c52f0
carlobearr <carlo_luis_v_santos@dlsu.edu.ph>
2020-03-22 17:17:27 +0800

    UI updates and mongoose added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express=require('express')
 const hbs = require("hbs")
+const mongoose = require('mongoose')
 const app = express()
 const port = 9090
+
+mongoose.connect('mongodb://localhost/nodekb')
+let db = mongoose.connection;
 
 app.set('view engine', 'hbs')
 
@@ -19,17 +23,32 @@ app.use(express.static(__dirname + '/public'));
 var usernames= "Default";
 var ids= "Default";
 app.get('/', function(req,res){res.render('index',{})})
-app.get('/post/:id(\d+)', function(req,res){res.render('post',{
-    ids: req.params.id,
-})})
-app.get('/post', function(req,res){res.render('post',{
-    forumtitle: '[Martial Law] What if Marcos never declared Martial Law?',
-    forumdate: 'February 28, 2020',
-    forumauthor: 'Carlobear',
-    forumpost:"What if Marcos never declared Martial Law and stepped down in '73? Would we have seen a President Ninoy Aquino? Would we have been a vastly different country today (socially and/or economically), or would it be more of the same? I'd like to know your thoughts on this.",
-    forumreact: 999,
-    commentcount: 2
-})})
+
+app.get('/signup', function(req,res){
+    res.render('registration',{})
+})
+app.get('/create_post', function(req,res){
+    res.render('createpost',{})
+})
+app.get('/viewall_post', function(req,res){
+    res.render('viewallpost',{})
+})
+
+app.get('/post/:id(\d+)', function(req,res){
+    res.render('post',{
+        ids: req.params.id,
+    })
+})
+app.get('/post', function(req,res){
+    res.render('post',{
+        forumtitle: '[Martial Law] What if Marcos never declared Martial Law?',
+        forumdate: 'February 28, 2020',
+        forumauthor: 'Carlobear',
+        forumpost:"What if Marcos never declared Martial Law and stepped down in '73? Would we have seen a President Ninoy Aquino? Would we have been a vastly different country today (socially and/or economically), or would it be more of the same? I'd like to know your thoughts on this.",
+        forumreact: 999,
+        commentcount: 2
+    })
+})
 app.listen(port, function(){
     console.log('App listening at port ' + port)
 })
